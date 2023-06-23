6ae83d50a5720e675669252f9d8c05b97a93bf9d
Sheethal Joshi <sh33thal24@gmail.com>
2023-06-24 00:59:03 +0530

    connected to db

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs')
 const app = express()
 
 require('dotenv').config()
@@ -10,12 +12,14 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
-
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
+    db()
     app.listen(PORT, () => {
-        console.log('listening to  port:', PORT)
-    });
+        console.log('listening to port:', PORT)
+    })
 }
 
-server ()
+server()
